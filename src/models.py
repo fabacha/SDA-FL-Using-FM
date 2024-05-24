85282ba1f60234616cca7f085f1247fe90121d40
Fatima <76473050+fabacha@users.noreply.github.com>
2024-05-24 16:15:01 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -130,36 +130,58 @@ class CNNFeMnist(nn.Module):
         #print(out.shape)
         return F.log_softmax(out, dim=1)
 
+# class CNNCifar(nn.Module):
+#     def __init__(self, args):
+#         super(CNNCifar, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(400, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, args.num_classes)
+#         self.feature_fc1 = None
+#         self.feature_fc2 = None
+#         self.feature_fc3 = None
+#         #self.feature_fc1_graph = None
+#         #self.feature_fc2_graph = None
+#         #self.feature_fc3_graph = None
+
+#     def forward(self, x):
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = x.view(x.size(0), -1)
+#         #self.feature_fc1_graph = x
+#         self.feature_fc1 = x.cpu().detach().numpy()
+#         x = F.relu(self.fc1(x))
+#         #self.feature_fc2_graph = x
+#         self.feature_fc2 = x.cpu().detach().numpy()
+#         x = F.relu(self.fc2(x))
+#         #self.feature_fc3_graph = x
+#         self.feature_fc3 = x.cpu().detach().numpy()
+#         x = self.fc3(x)
+#         return F.log_softmax(x, dim=1)
+
+#MyCNN
 class CNNCifar(nn.Module):
-    def __init__(self, args):
+    def __init__(self):
         super(CNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(400, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, args.num_classes)
-        self.feature_fc1 = None
-        self.feature_fc2 = None
-        self.feature_fc3 = None
-        #self.feature_fc1_graph = None
-        #self.feature_fc2_graph = None
-        #self.feature_fc3_graph = None
+        self.fc1 = nn.Linear(128 * 8 * 8, 128)  # Adjusted based on CIFAR-10 image size
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
         x = x.view(x.size(0), -1)
-        #self.feature_fc1_graph = x
-        self.feature_fc1 = x.cpu().detach().numpy()
         x = F.relu(self.fc1(x))
-        #self.feature_fc2_graph = x
-        self.feature_fc2 = x.cpu().detach().numpy()
-        x = F.relu(self.fc2(x))
-        #self.feature_fc3_graph = x
-        self.feature_fc3 = x.cpu().detach().numpy()
-        x = self.fc3(x)
-        return F.log_softmax(x, dim=1)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
 
 class CNNORL(nn.Module):
     def __init__(self):
