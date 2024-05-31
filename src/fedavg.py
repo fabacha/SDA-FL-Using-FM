c5fe3618549cc1fb05d80f31589bc368821c2be5
Fatima <76473050+fabacha@users.noreply.github.com>
2024-05-31 16:11:10 +0100

    Update fedavg.py

--- src/fedavg.py
+++ src/fedavg.py
@@ -41,6 +41,8 @@ if __name__ == '__main__':
         elif args.dataset == 'cifar':
             global_model = CNNCifar()
             #global_model = CNNCifar(args=args)
+        elif args.dataset == 'cifar10_extr_noniid:
+            global_model = CNNCifar()
     else:
         exit('Error: unrecognized model')
 
