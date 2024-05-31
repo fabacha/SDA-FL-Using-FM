0f4be90c249ef59e51bf0ae8027cd6401df8a093
Fatima <76473050+fabacha@users.noreply.github.com>
2024-05-31 17:50:22 +0100

    Update sampling.py

--- src/sampling.py
+++ src/sampling.py
@@ -205,24 +205,86 @@ def cifar_noniid(dataset, num_users):
     return dict_users
 
 
-def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_samples, rate_unbalance):
+# def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_samples, rate_unbalance):
+#     num_shards_train, num_imgs_train = int(50000/num_samples), num_samples
+#     num_classes = 10
+#     num_imgs_perc_test, num_imgs_test_total = 1000, 10000
+#     assert(n_class * num_users <= num_shards_train)
+#     assert(n_class <= num_classes)
+#     idx_class = [i for i in range(num_classes)]
+#     idx_shard = [i for i in range(num_shards_train)]
+#     dict_users_train = {i: np.array([]) for i in range(num_users)}
+#     dict_users_test = {i: np.array([]) for i in range(num_users)} 
+#     idxs = np.arange(num_shards_train*num_imgs_train)
+#     # labels = dataset.train_labels.numpy()
+#     labels = np.array(train_dataset.targets)
+#     idxs_test = np.arange(num_imgs_test_total)
+#     labels_test = np.array(test_dataset.targets)
+#     #labels_test_raw = np.array(test_dataset.targets)
+
+#     # sort labels
+#     idxs_labels = np.vstack((idxs, labels))
+#     idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+#     idxs = idxs_labels[0, :]
+#     labels = idxs_labels[1, :]
+
+#     idxs_labels_test = np.vstack((idxs_test, labels_test))
+#     idxs_labels_test = idxs_labels_test[:, idxs_labels_test[1, :].argsort()]
+#     idxs_test = idxs_labels_test[0, :]
+#     #print(idxs_labels_test[1, :])
+
+
+#     # divide and assign
+#     for i in range(num_users):
+#         user_labels = np.array([])
+#         rand_set = set(np.random.choice(idx_shard, n_class, replace=False))
+#         idx_shard = list(set(idx_shard) - rand_set)
+#         unbalance_flag = 0
+#         for rand in rand_set:
+#             if unbalance_flag == 0:
+#                 dict_users_train[i] = np.concatenate(
+#                     (dict_users_train[i], idxs[rand*num_imgs_train:(rand+1)*num_imgs_train]), axis=0)
+#                 user_labels = np.concatenate((user_labels, labels[rand*num_imgs_train:(rand+1)*num_imgs_train]), axis=0)
+#             else:
+#                 dict_users_train[i] = np.concatenate(
+#                     (dict_users_train[i], idxs[rand*num_imgs_train:int((rand+rate_unbalance)*num_imgs_train)]), axis=0)
+#                 user_labels = np.concatenate((user_labels, labels[rand*num_imgs_train:int((rand+rate_unbalance)*num_imgs_train)]), axis=0)
+#             unbalance_flag = 1
+#         user_labels_set = set(user_labels)
+#         #print(user_labels_set)
+#         #print(user_labels)
+#         for label in user_labels_set:
+#             dict_users_test[i] = np.concatenate((dict_users_test[i], idxs_test[int(label)*num_imgs_perc_test:int(label+1)*num_imgs_perc_test]), axis=0)   
+#         #print(set(labels_test_raw[dict_users_test[i].astype(int)]))
+
+#     return dict_users_train, dict_users_test
+
+def get_dataset_cifar10_extr_noniid(num_users, class_distribution, nsamples, rate_unbalance):
+    data_dir = '../data/cifar/'
+    apply_transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
+                                     transform=apply_transform)
+
+    test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
+                                    transform=apply_transform)
+
+    user_groups_train, user_groups_test = cifar_extr_noniid(train_dataset, test_dataset, num_users, class_distribution, nsamples, rate_unbalance)
+    return train_dataset, test_dataset, user_groups_train, user_groups_test
+
+def cifar_extr_noniid(train_dataset, test_dataset, num_users, class_distribution, num_samples, rate_unbalance):
     num_shards_train, num_imgs_train = int(50000/num_samples), num_samples
     num_classes = 10
     num_imgs_perc_test, num_imgs_test_total = 1000, 10000
-    assert(n_class * num_users <= num_shards_train)
-    assert(n_class <= num_classes)
-    idx_class = [i for i in range(num_classes)]
     idx_shard = [i for i in range(num_shards_train)]
     dict_users_train = {i: np.array([]) for i in range(num_users)}
-    dict_users_test = {i: np.array([]) for i in range(num_users)} 
+    dict_users_test = {i: np.array([]) for i in range(num_users)}
     idxs = np.arange(num_shards_train*num_imgs_train)
-    # labels = dataset.train_labels.numpy()
     labels = np.array(train_dataset.targets)
     idxs_test = np.arange(num_imgs_test_total)
     labels_test = np.array(test_dataset.targets)
-    #labels_test_raw = np.array(test_dataset.targets)
 
-    # sort labels
     idxs_labels = np.vstack((idxs, labels))
     idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
     idxs = idxs_labels[0, :]
@@ -231,31 +293,20 @@ def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_sampl
     idxs_labels_test = np.vstack((idxs_test, labels_test))
     idxs_labels_test = idxs_labels_test[:, idxs_labels_test[1, :].argsort()]
     idxs_test = idxs_labels_test[0, :]
-    #print(idxs_labels_test[1, :])
 
-
-    # divide and assign
     for i in range(num_users):
         user_labels = np.array([])
-        rand_set = set(np.random.choice(idx_shard, n_class, replace=False))
-        idx_shard = list(set(idx_shard) - rand_set)
-        unbalance_flag = 0
-        for rand in rand_set:
-            if unbalance_flag == 0:
-                dict_users_train[i] = np.concatenate(
-                    (dict_users_train[i], idxs[rand*num_imgs_train:(rand+1)*num_imgs_train]), axis=0)
-                user_labels = np.concatenate((user_labels, labels[rand*num_imgs_train:(rand+1)*num_imgs_train]), axis=0)
-            else:
-                dict_users_train[i] = np.concatenate(
-                    (dict_users_train[i], idxs[rand*num_imgs_train:int((rand+rate_unbalance)*num_imgs_train)]), axis=0)
-                user_labels = np.concatenate((user_labels, labels[rand*num_imgs_train:int((rand+rate_unbalance)*num_imgs_train)]), axis=0)
-            unbalance_flag = 1
-        user_labels_set = set(user_labels)
-        #print(user_labels_set)
-        #print(user_labels)
-        for label in user_labels_set:
-            dict_users_test[i] = np.concatenate((dict_users_test[i], idxs_test[int(label)*num_imgs_perc_test:int(label+1)*num_imgs_perc_test]), axis=0)   
-        #print(set(labels_test_raw[dict_users_test[i].astype(int)]))
+        chosen_classes = class_distribution[i]
+        for cls in chosen_classes:
+            cls_idx = np.where(labels == cls)[0]
+            num_cls_samples = len(cls_idx)
+            selected_idx = np.random.choice(cls_idx, int(num_samples / len(chosen_classes)), replace=False)
+            dict_users_train[i] = np.concatenate((dict_users_train[i], selected_idx))
+            user_labels = np.concatenate((user_labels, labels[selected_idx]))
+        for cls in chosen_classes:
+            cls_test_idx = np.where(labels_test == cls)[0]
+            selected_test_idx = np.random.choice(cls_test_idx, int(num_imgs_perc_test / len(chosen_classes)), replace=False)
+            dict_users_test[i] = np.concatenate((dict_users_test[i], selected_test_idx))
 
     return dict_users_train, dict_users_test
 
